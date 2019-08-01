c8f565d0108963daaacbe663873f3f69d22e0707
Shriram V <shri314@yahoo.com>
2019-08-01 15:37:45 +0530

    Removed javacomplete2 vim plugin

--- install.sh
+++ install.sh
@@ -146,7 +146,8 @@ RawGetTool ~/.vim/autoload "https://raw.githubusercontent.com/tpope/vim-pathogen
 
 VimBundle https://github.com/zirrostig/vim-schlepp.git
 VimBundle https://github.com/airblade/vim-gitgutter.git
-#VimBundle https://github.com/artur-shaik/vim-javacomplete2
+#VimBundle https://github.com/artur-shaik/vim-javacomplete2.git
+#VimBundle https://github.com/mzlogin/vim-markdown-toc.git
 
 # set version - we plan to use this for upgrades
 if [ ! -f ~/.version-env-scripts ]
